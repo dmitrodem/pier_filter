2ad97604c5c6f0ceff7e788d9d0de0b1c7f54809
Dmitriy Dyomin <dmitrodem@gmail.com>
2017-06-21 19:45:28 +0300

    Довел до согласия результатов с HFSS

--- mode_matching/mode_matching.py
+++ mode_matching/mode_matching.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2
-
+# -*- coding: utf-8 -*-
 import numpy as np
 import numpy.linalg as lin
 import matplotlib.pyplot as plt
 import sys
+from scipy.integrate import quad
 
-a = 1.0
+# assuming a < b
+
+a = np.sqrt(0.5)
 b = 1.0
 f = 1.0 # f/c0 actually, f_c = 1/(2a)
+
+assert (a < b)
 
 M = 1
 N = M
@@ -15,17 +20,30 @@ N = M
 DTYPE = np.complex
 
 def eigenfunction(a, n, x):
+    """
+    собственная функция f_a^n(x) прямоугольного волновода, нормированная на 1
+    учитываем только
+    1. TE-волны
+    2. зависимость от y отсутствует (m = 0)
+    3. только нечетные моды (n = 2 n' + 1)
+    """
     n = 2*n + 1
     return np.sqrt(2.0/a) * np.cos(np.pi*n*x/a)
 
 def _V(m, n):
+    """
+    матричный элемент V_{m, n} = \int_\limits_{-a/2}^{a/2} f_a^m(x) f_b^n(x) dx
+    """
     global a
     global b
     _m = m
     m = DTYPE(2*m + 1)
     n = DTYPE(2*n + 1)
-    arg = np.real(n * a/b) % 4
-    return 2.0/np.pi/np.sqrt(a*b) * (-1)**_m * (1.0/(m/a + n/b) + 1.0/(m/a - n/b)) * np.cos(arg * np.pi/2)
+    if m/a == n/b:
+        result = np.sqrt(a/b)
+    else:
+        result = 4./np.pi * a**(-1.5) * b**(-0.5) * m * (-1)**(_m) * np.cos(0.5 * np.pi * a/b * n) / ((m/a)**2 - (n/b)**2)
+    return result
 
 def _Y(n, a):
     global f
@@ -85,8 +103,6 @@ def conv_to_S(R):
     return result
 
 S = conv_to_S(R)
-plt.matshow(np.abs(S))
-plt.show()
 
 A = np.zeros(2*M)
 A[0] = 1.0
@@ -102,7 +118,8 @@ Eb = np.zeros(xb.shape, dtype = DTYPE)
 for i in xrange(M):
     Eb += (A[M+i] + B[M+i]) * eigenfunction(b, i, xb)
 
-plt.plot(xa, Ea)
-plt.plot(xb, Eb)
-plt.grid()
-plt.show()
+
+
+testfunc = lambda x: eigenfunction(a, 11, x) * eigenfunction(b, 17, x)
+print "Q = ", quad(testfunc, -a/2, a/2)[0]
+print "P = ", _V(11, 17)
--- mode_matching/mode_solve.py
+++ mode_matching/mode_solve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 from pylab import *
 from sys import stdout
@@ -6,50 +6,78 @@ from time import time
 
 Mmax = 10
 Nmax = 10
-a = 1.0+0j
+a = 1.0001+0j
 b = 0.5001+0j
-kv = 2.5*pi*a
+kv = 2.5*a
 
 def Imn(m, n, a, b):
-    return 2*(-1)**(1+n)*a**2*b*cos(0.5*pi*b*(2*m-1)/a)*(2*n-1)/pi/(-b**2*(2*m-1)**2+a**2*(2*n-1)**2)
+    return 4*(-1)**(n)*a**1.5*b**0.5*cos(0.5*pi*b*(2*m+1)/a)*(2*n+1)/pi/(-b**2*(2*m+1)**2+a**2*(2*n+1)**2)
 
 def beta(n, l, kv):
-    return sqrt(kv**2-pi**2*(2*n-1)**2/l**2)
+    return sqrt(1.0-((2*n+1)/l/kv)**2)
 
 t0 = time()
 s11 = zeros(1001)
 i = 0
-kv_space = linspace(0, 5*pi/a, 1001)
-for kv in kv_space:
-    Q = fromfunction(lambda m, n: 2.0/a*Imn(m+1, n+1, a, b), (Mmax, Nmax))
-    P = fromfunction(lambda m, n: 2.0/b*beta(n+1, a, kv)/beta(m+1, b, kv)*Imn(n+1, m+1, a, b), (Nmax, Mmax))
+kv_space = linspace(0, 5/a, 1001)
+kv_space = array([0j, 3 + 0j])
+for kv in kv_space[1:]:
+    Q = fromfunction(lambda m, n: Imn(m, n, a, b), (Mmax, Nmax))
+    P = fromfunction(lambda m, n: beta(n, a, kv)/beta(m, b, kv)*Imn(n, m, a, b), (Nmax, Mmax))
 
-    M = dot(inv(eye(Mmax) + dot(Q, P)), (-eye(Mmax) + dot(Q, P)))
+    M = dot(inv(eye(Mmax, dtype=complex) + dot(Q, P)), (-eye(Mmax, dtype=complex) + dot(Q, P)))
     s11[i] = 20*log10(abs(M[0,0]))
     i += 1
 
-plot(kv_space/pi*a, s11)
-grid()
-show()
+# plot(kv_space*a, s11)
+# grid()
+# show()
 
 ai = zeros(Mmax, dtype=complex); ai[0] = 1.0
 ar = dot(M, ai)
 br = dot(P, ai - ar)
 
-print(ar)
-print(br)
 
 xs = linspace(-0.5*a, 0.5*a, 1001)
-lhs = zeros(xs.shape, dtype=complex)
-rhs = zeros(xs.shape, dtype=complex)
+lhsE = zeros(xs.shape, dtype=complex)
+rhsE = zeros(xs.shape, dtype=complex)
+lhsH = zeros(xs.shape, dtype=complex)
+rhsH = zeros(xs.shape, dtype=complex)
+lhsS = 0j
+rhsS = 0j
 
 for m in range(Mmax):
-    lhs += ar[m]*cos(pi/a*(2*m+1)*xs)
+    lhsE +=  ai[m]*cos(pi/a*(2*m+1)*xs)*sqrt(2.0/a)
+    lhsE +=  ar[m]*cos(pi/a*(2*m+1)*xs)*sqrt(2.0/a)
+    lhsH +=  beta(m, a, kv) * ai[m]*cos(pi/a*(2*m+1)*xs)*sqrt(2.0/a)
+    lhsH += -beta(m, a, kv) * ar[m]*cos(pi/a*(2*m+1)*xs)*sqrt(2.0/a)
+    lhsS += beta(m, a, kv)*(ai[m]**2 - ar[m]**2)
     
 for n in range(Nmax):
-    rhs += br[n]*cos(pi/b*(2*n+1)*xs)
+    rhsE += br[n]*cos(pi/b*(2*n+1)*xs)*sqrt(2.0/b)
+    rhsH += beta(n, b, kv) * br[n]*cos(pi/b*(2*n+1)*xs)*sqrt(2.0/b)
+    rhsS += beta(n, b, kv) * br[n]**2
 
-plot(xs, abs(cos(pi/a*xs)+lhs))
-plot(xs, abs(rhs))
+subplot(211)
+plot(xs, abs(lhsE))
+plot(xs, abs(rhsE))
+grid()
+subplot(212)
+plot(xs, abs(lhsH))
+plot(xs, abs(rhsH))
 grid()
 show()
+
+delta = (lhsS - rhsS) / (lhsS + rhsS) * 2
+print "Energy imbalance = ", np.abs(delta)
+# np.set_printoptions(precision=3, suppress=True, linewidth=250)
+# print("Showing table P : ")
+# print(abs(P))
+
+with open("/tmp/Q.npy", "w") as fd:
+    np.save(fd, Q)
+
+print "S11 = ", 20*log10(abs(M[0, 0]))
+print "ang(S11) = ", np.rad2deg(np.angle(M[0,0]))
+print "S11 = ", 20*log10(abs(M[0, 1]))
+print "ang(S11) = ", np.rad2deg(np.angle(M[0,1]))
